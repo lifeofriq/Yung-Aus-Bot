bb013b40ba7aad83ea2d0fd40cfb04c5a8700678
lifeofriq <93238351+lifeofriq@users.noreply.github.com>
2022-01-12 10:24:59 +0700

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+import Music
+
+for i in range(len(cogs)):
+    cogs[i].setup()
+
+
+client = commands.Bot(command_prefix='?', intents = discord, intents = discord.Intents.all())
+client.run(OTMwNjQ1Njc5ODI1NjQ1NTk4.Yd45VA.ZhslKq8s17MCm1oULzS9X3tWk08)
---
+++ Music.py
@@ -0,0 +1,15 @@
+from _typeshed import Self
+import discord
+from discord.ext import commands 
+input youtube_dl
+
+class music(commands.cog):
+    def_init_(self, client)
+    self.client = client
+
+
+
+
+
+def setup(client)
+client.add_cog(music(client))
